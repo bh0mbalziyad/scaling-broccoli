8a235d1362b3da2ed53ea465eed27667d98bfd9c
bh0mbalziyad <zeebhombal@gmail.com>
2020-08-01 21:57:52 +0530

    Added styles to the categories grid view

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Deli Meal',
+        ),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(15),
+        children: DUMMY_DATA
+            .map((catData) => CategoryItem(
+                  catData.title,
+                  catData.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
